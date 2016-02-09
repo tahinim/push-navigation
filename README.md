bce59494923d0c2d30e75568a3da3369dfc84366
tahinim <tahinim@hotmail.com>
2016-02-09 16:11:28 -0500

    added previous files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# push-navigation
+# Push Menus
+
+
+
---
+++ js/push-menu.js
@@ -0,0 +1,20 @@
+// Needs latest version of jQuery to run
+$(function(){
+  $('body').removeClass('noscript');
+  
+  $('.toggle-btn, .close-btn').click(function(){
+    toggleNav();
+  });
+});
+
+
+function toggleNav() {
+ if ($('.site-wrapper').attr('data-state') == 'slide-closed') {
+  
+ $('.site-wrapper').attr('data-state','slide-open');   
+ }else{
+ 
+ $('.site-wrapper').attr('data-state','slide-closed'); 
+     
+ }
+}
